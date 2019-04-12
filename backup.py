c6a5c532f85c3fd62b9eefcd0d4ae9ca40b3b56f
yipwing <yip_kof@hotmail.com>
2019-04-12 13:19:44 +0800

    add some condition.

--- backup.py
+++ backup.py
@@ -7,37 +7,17 @@ move_command = 'mv `ls %s|grep -v "%s"` %s && ll'
 
 # TODO finish this.
 def MovingPath(path, dest, exception):
-    print(move_command % (path, exception, dest))
-    value = raw_input("are you sure you want to move %s to %s ?" % (path, dest))
-    if value == 'yes' or value == 'y':
-        if os.path.exists(dest):
-            status = os.system(move_command % (path, exception, dest))
-            print(status)
-        else:
-            os.makedirs(dest, 755)
-            status = os.system(move_command % (path, exception, dest))
-            print(status)
-        # if os.path.exists(path):
-        #     if os.path.exists(dest):
-        #         try:
-        #             shutil.move(path, dest)
-        #         except:
-        #             print("moving path is failed, please contact administrator.")
-        #     else:
-        #         os.makedirs(dest, 755)
-        #         try:
-        #             shutil.move(path, dest)
-        #         except:
-        #             print("moving path is failed, please contact administrator.")
-        # if exception:
-        #     exceptFile = os.path.dirname(dest) + "/" + exception
-        #     if os.path.isfile(exceptFile):
-        #         try:
-        #             shutil.move(exceptFile, path)
-        #         except:
-        #             print("moving path is failed, please contact administrator.")
-        # print("source directory doesn't exists")
-    pass
+    if os.path.exists(path):
+        value = raw_input("are you sure you want to move %s to %s ?" % (path, dest))
+        if value == 'yes' or value == 'y':
+            if os.path.exists(dest):
+                status = os.system(move_command % (path, exception, dest))
+                print(status)
+            else:
+                os.makedirs(dest, 755)
+                status = os.system(move_command % (path, exception, dest))
+                print(status)
+    print("the source directory doesn't exists\nmake sure your input source path is exists.")
     
 if __name__ == "__main__":
     print(os.path.dirname(sys.argv[2]) + "/" + sys.argv[3])
